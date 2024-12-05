b51b4e853e172739ebb1c9aab107422e393f5c05
Stanislovas Kujalis <stanislovas@pikta.lt>
2024-12-05 18:08:15 +0200

    day 5 cleaned up

--- 05.py
+++ 05.py
@@ -99,45 +99,12 @@ def fix_pages(rules_dict, update):
         for aa in a:
             G.add_edge(m, aa)
 
-    full = {k: (nx.ancestors(G, k), nx.descendants(G, k)) for k in G.nodes()}
     for node in G.nodes():
         if len(nx.ancestors(G, node)) == len(nx.descendants(G, node)):
             return node
 
 
-def safe_index(li, item):
-    try:
-        return li.index(item)
-    except ValueError:
-        return -1
-
-
-def build_order(data):
-    rules, updates = data.split("\n\n")
-
-    rules_dict = defaultdict(lambda: (set(), set()))
-    G = nx.DiGraph()
-
-    for rule in rules.splitlines():
-        before, after = rule.split('|')
-        before, after = int(before), int(after)
-        rules_dict[after][0].add(before)
-        rules_dict[before][1].add(after)
-        G.add_edge(before, after)
-
-    # for k, v in rules_dict.items():
-    #     print(k)
-    #     print(v[0])
-    #     print(v[1])
-
-    # pos = nx.planar_layout(G)
-    # nx.draw_networkx_edges(G, pos, arrows=True)
-    # plt.show()
-    print(nx.is_directed_acyclic_graph(G))
-
-
 with open("05.txt") as inf:
     data = inf.read()
-    # print(part_one(data))
+    print(part_one(data))
     print(part_two(data))
-    # build_order(data)
